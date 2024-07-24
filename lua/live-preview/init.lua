041a2ca9eb7107d4c7f2b76814db62c5db0b7dc0
brianhuster <phambinhanctb2004@gmail.com>
2024-07-24 23:47:21 +0700

    update

--- lua/live-preview/init.lua
+++ lua/live-preview/init.lua
@@ -1,75 +1,86 @@
--- ~/.local/share/nvim/lazy/live-preview.nvim/live-preview.lua
-
 local M = {}
 
+-- Function to stop any process using port 3000
 function M.stop_preview()
-  local port = 3000
-  if vim.fn.has("win32") == 1 then
-    local kill_command = string.format(
-      "netstat -ano | findstr :%d | findstr LISTENING | for /F \"tokens=5\" %%i in ('more') do taskkill /F /PID %%i",
-      port
-    )
-    os.execute(kill_command)
-  else
-    local kill_command = string.format(
-      "lsof -t -i:%d | xargs -r kill -9",
-      port
-    )
-    os.execute(kill_command)
-  end
+    local port = 3000
+    if vim.fn.has("win32") == 1 then
+        local kill_command = string.format(
+            "netstat -ano | findstr :%d | findstr LISTENING | for /F \"tokens=5\" %%i in ('more') do taskkill /F /PID %%i",
+            port
+        )
+        os.execute(kill_command)
+    else
+        local kill_command = string.format(
+            "lsof -t -i:%d | xargs -r kill -9",
+            port
+        )
+        os.execute(kill_command)
+    end
 end
 
-
+-- Function to preview the file
 function M.preview_file()
     print("live-preview called")
     local filename = vim.fn.expand('%:p')
     if not filename or filename == "" then
-    print("No file is open")
-    return
+        print("No file is open")
+        return
     end
 
     local extname = vim.fn.fnamemodify(filename, ":e")
     local supported_exts = { "md", "html" }
 
     if not vim.tbl_contains(supported_exts, extname) then
-    print("Unsupported file type")
-    return
+        print("Unsupported file type")
+        return
     end
 
     -- Start nodemon server
     M.stop_preview()
+
+    local log_file = vim.fn.stdpath('data') .. '/lazy/live-preview.nvim/log.txt'
     local command = string.format("nodemon --exec 'node ~/.local/share/nvim/lazy/live-preview.nvim/server.js %s'", filename)
+
     vim.fn.jobstart(command, {
-        os.execute('echo > ~/.local/share/nvim/lazy/live-preview.nvim/log.txt')
+        stdout_buffered = true,
+        stderr_buffered = true,
         on_stdout = function(_, data)
-        if data then
-            for _, line in ipairs(data) do
-              print("stdout: " .. line)
-              os.execute('echo "' .. line .. '" >> ~/.local/share/nvim/lazy/live-preview.nvim/log.txt')
+            if data then
+                for _, line in ipairs(data) do
+                    print("stdout: " .. line)
+                    local file = io.open(log_file, "a")
+                    if file then
+                        file:write("stdout: " .. line .. "\n")
+                        file:close()
+                    end
+                end
             end
-          end
         end,
         on_stderr = function(_, data)
-          if data then
-            for _, line in ipairs(data) do
-              print("stderr: " .. line)
-              os.execute('echo "' .. line .. '" >> ~/.local/share/nvim/lazy/live-preview.nvim/log.txt')
+            if data then
+                for _, line in ipairs(data) do
+                    print("stderr: " .. line)
+                    local file = io.open(log_file, "a")
+                    if file then
+                        file:write("stderr: " .. line .. "\n")
+                        file:close()
+                    end
+                end
             end
-          end
         end,
         on_exit = function(_, code)
-          if code ~= 0 then
-            print("Error starting the server")
-          end
+            if code ~= 0 then
+                print("Error starting the server")
+            end
         end
     })
 
     -- Open browser with the rendered file
     local open_browser_command = "xdg-open"
     if vim.fn.has("mac") == 1 then
-    open_browser_command = "open"
+        open_browser_command = "open"
     elseif vim.fn.has("win32") == 1 then
-    open_browser_command = "start"
+        open_browser_command = "start"
     end
 
     os.execute(open_browser_command .. " http://localhost:3000")
