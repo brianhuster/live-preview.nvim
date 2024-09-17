bc18a91ea7d8f875090546c1e6495e06827103d6
brianhuster <phambinhanctb2004@gmail.com>
2024-09-17 08:14:56 +0700

    update

--- server/public/ws_script.js
+++ server/public/ws_script.js
@@ -32,7 +32,7 @@ window.onload = () => {
     connectWebSocket();
     setInterval(() => {
         if (!connected) {
-            connectWebSocket();
+            //connectWebSocket();
             if (connected) {
                 window.location.reload();
             }
