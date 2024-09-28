28504c8a1974bbd3364b36b0ed7fce3a2610165d
brianhuster <phambinhanctb2004@gmail.com>
2024-09-28 12:38:34 +0700

    update README

--- static/markdown/main.js
+++ static/markdown/main.js
@@ -1,7 +1,9 @@
-marked.setOptions({
-    sanitize: false,
-});
+const renderer = new marked.Renderer();
+renderer.code = (code, language) => {
+    return `<pre class="language-${language}">${code}</pre>`;
+};
 
+marked.use({ renderer });
 
 const markdownText = document.querySelector('.markdown-body').innerHTML;
 const html = marked.parse(markdownText);
