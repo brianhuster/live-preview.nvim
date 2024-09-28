acca36e56d9ff1cd5a4c86ccfed59a4c3856c1a0
brianhuster <phambinhanctb2004@gmail.com>
2024-09-28 12:09:02 +0700

    update README

--- static/markdown/main.js
+++ static/markdown/main.js
@@ -1,32 +1,5 @@
-const renderer = new marked.Renderer();
-
-renderer.code = function(code, language, escaped = false) {
-    console.log("Original code:", code);
-    console.log("Language:", language);
-
-    if (this.options.highlight) {
-        const out = this.options.highlight(code, language);
-        console.log("Highlighted code:", out);
-        if (out != null && out !== code) {
-            escaped = true;
-            code = out;
-        }
-    }
-
-    if (!language) {
-        return '<pre><code>' + code + '</code></pre>';
-    }
-
-    return '<pre><code class="language-' + language + '">' + code + '</code></pre>\n';
-};
-
 marked.setOptions({
-    renderer: renderer,
-    sanitize: false, // Disable sanitization to prevent escaping
-    highlight: function(code, language) {
-        // You can add your syntax highlighting logic here
-        return code;
-    }
+    sanitize: false,
 });
 
 
