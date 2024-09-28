2735ed86e0e05b37f08ff03617f22685adf717b8
brianhuster <phambinhanctb2004@gmail.com>
2024-09-28 16:15:29 +0700

    update action.js

--- action.js
+++ action.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 
 const packspec = JSON.parse(fs.readFileSync('pkg.json', 'utf8'));
+console.log(packspec.name, packspec.version, packspec.engines.nvim);
 
 const updateReadme = (file) => {
     let readme = fs.readFileSync(file, 'utf8');
 
     // Update plugin name and version
-    const nameVersionRegex = /# \w+ \d+\.\d+\.\d+/;
+    const nameVersionRegex = /^# [\w.-]+/m;
+    console.log(readme.match(nameVersionRegex));
     readme = readme.replace(nameVersionRegex, `# ${packspec.name} ${packspec.version}`);
 
     // Update Neovim requirements
-    const nvimVersionRegex = /- Neovim \d+\.\d+\.\d+/;
-    readme = readme.replace(nvimVersionRegex, `- Neovim ${packspec.engines.nvim}`);
+    const nvimVersionRegex = /^- Neovim.*/m;
+    console.log(readme.match(nvimVersionRegex));
+    readme = readme.replace(nvimVersionRegex, `- Neovim${packspec.engines.nvim}`);
 
     fs.writeFileSync(file, readme);
 
@@ -19,7 +22,7 @@ const updateReadme = (file) => {
 };
 
 // List all files in the current directory
-const files = fs.readdirSync('.'); 
+const files = fs.readdirSync('.');
 
 const readmeFiles = files.filter(file => file === 'README.md' || file.match(/^README\..*\.md$/));
 
