68c8f658d130ff292bb3b4ac45f0a992ee7d04d9
brianhuster <phambinhanctb2004@gmail.com>
2024-09-28 10:22:11 +0700

    update web.lua

--- static/markdown/main.js
+++ static/markdown/main.js
@@ -1,8 +1,21 @@
 const renderer = new marked.Renderer();
-renderer.code = function(code, language) {
-    const langClass = language ? `language-${language}` : '';
-    return `<pre><code class="${langClass}">${code}</code></pre>`;
+
+renderer.code = function(code, language, escaped = false) {
+    if (this.options.highlight) {
+        const out = this.options.highlight(code, language);
+        if (out != null && out !== code) {
+            escaped = true;
+            code = out;
+        }
+    }
+
+    if (!language) {
+        return '<pre><code>' + code + '</code></pre>';
+    }
+
+    return '<pre><code class="language-' + language + '">' + code + '</code></pre>\n';
 };
+
 
 marked.setOptions({
     renderer: renderer,
