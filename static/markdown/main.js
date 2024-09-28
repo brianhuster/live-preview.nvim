95ae1a42e8ae75593109cfc6cb414cc43cea98d6
brianhuster <phambinhanctb2004@gmail.com>
2024-09-28 11:51:26 +0700

    update web.lua

--- static/markdown/main.js
+++ static/markdown/main.js
@@ -1,3 +1,26 @@
+const renderer = new marked.Renderer();
+
+renderer.code = function(code, language, escaped = false) {
+    console.log("Original code:", code);
+    console.log("Language:", language);
+
+    if (this.options.highlight) {
+        const out = this.options.highlight(code, language);
+        console.log("Highlighted code:", out);
+        if (out != null && out !== code) {
+            escaped = true;
+            code = out;
+        }
+    }
+
+    if (!language) {
+        return '<pre><code>' + code + '</code></pre>';
+    }
+
+    return '<pre><code class="language-' + language + '">' + code + '</code></pre>\n';
+};
+
+
 const markdownText = document.querySelector('.markdown-body').innerHTML;
 const html = marked.parse(markdownText);
 console.log(html);
