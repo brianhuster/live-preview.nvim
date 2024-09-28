0f616f877221d126b6c57d4f5028dd0c17328774
brianhuster <phambinhanctb2004@gmail.com>
2024-09-28 14:12:44 +0700

    fix bug

--- static/asciidoc/main.js
+++ static/asciidoc/main.js
@@ -2,6 +2,7 @@ import Asciidoctor from '/live-preview.nvim/static/asciidoc/asciidoctor.min.js'
 const asciidoctor = Asciidoctor();
 const adoc = document.querySelector('.markdown-body').innerHTML;
 const html = asciidoctor.convert(adoc);
+console.log(html);
 document.querySelector('.markdown-body').innerHTML = html;
 
 document.addEventListener("DOMContentLoaded", function() {
