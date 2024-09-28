cf81e265462863372981393ffaebbd47d73741a5
brianhuster <phambinhanctb2004@gmail.com>
2024-09-28 10:28:48 +0700

    update web.lua

--- static/markdown/main.js
+++ static/markdown/main.js
@@ -1,25 +1,3 @@
-const renderer = new marked.Renderer();
-
-renderer.code = function(code, language, escaped = false) {
-    if (this.options.highlight) {
-        const out = this.options.highlight(code, language);
-        if (out != null && out !== code) {
-            escaped = true;
-            code = out;
-        }
-    }
-
-    if (!language) {
-        return '<pre><code>' + code + '</code></pre>';
-    }
-
-    return '<pre><code class="language-' + language + '">' + code + '</code></pre>\n';
-};
-
-
-marked.setOptions({
-    renderer: renderer,
-});
 const markdownText = document.querySelector('.markdown-body').innerHTML;
 const html = marked.parse(markdownText);
 console.log(html);
