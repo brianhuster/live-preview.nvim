e6f328f467cbcc5b894221d75b09d223c6d8cca6
brianhuster <phambinhanctb2004@gmail.com>
2024-10-05 08:03:16 +0700

    update test

---
+++ tests/plugin_spec.lua
@@ -0,0 +1,34 @@
+local live_preview = require("live-preview")
+
+describe("Live Preview", function()
+	describe("setup", function()
+		it("should be accessible", function()
+			local status, err = pcall(function()
+				live_preview.setup({
+					commands = {
+						start = "LivePreview",
+						stop = "StopPreview"
+					},
+					port = 5500,
+					browser = "default"
+				})
+			end)
+			assert.is_true(status) -- Kiểm tra rằng hàm setup không gây lỗi
+		end)
+	end)
+end)
+
+describe("Server", function()
+	local server = live_preview.server.Server:new("path/to/webroot")
+	describe("new", function()
+		it("should be accessible", function()
+			assert.is_not_nil(server.server)
+			assert.is_not_nil(server.webroot)
+			assert.is_not_nil(server:new())
+			assert.is_not_nil(server:stop())
+			assert.is_not_nil(server:start())
+			assert.is_not_nil(server:routes())
+			assert.is_not_nil(server:watch_dir())
+		end)
+	end)
+end)
