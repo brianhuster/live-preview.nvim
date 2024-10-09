56a4c74a7f31f5cc29fb79e17eccd76f7e979be8
brianhuster <phambinhanctb2004@gmail.com>
2024-10-09 16:09:13 +0700

    update

--- plugin/livepreview.lua
+++ plugin/livepreview.lua
@@ -1,8 +1,8 @@
-local spec = require("live-preview.spec")
+local spec = require("livepreview.spec")
 local nvim_ver_range = spec().engines.nvim
 local nvim_ver_table = vim.version()
 local nvim_ver = string.format("%d.%d.%d", nvim_ver_table.major, nvim_ver_table.minor, nvim_ver_table.patch)
-local is_compatible = require("live-preview.health").is_compatible
+local is_compatible = require("livepreview.health").is_compatible
 
 if not is_compatible(nvim_ver, nvim_ver_range) then
 	vim.notify_once("Live Preview requires Neovim " .. nvim_ver_range .. ", but you are using " .. nvim_ver)
@@ -10,8 +10,6 @@ end
 
 vim.filetype.add({
 	pattern = {
-		[".*/live%-preview%.nvim/doc/.+%.txt"] = "help"
+		[".*/livepreview%.nvim/doc/.+%.txt"] = "help"
 	},
 })
-
-
