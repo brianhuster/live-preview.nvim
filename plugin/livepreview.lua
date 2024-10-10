eae8dab4272be75a0bdd23487e60aeb35cc61fa2
brianhuster <phambinhanctb2004@gmail.com>
2024-10-10 14:04:51 +0700

    support native package

--- plugin/livepreview.lua
+++ plugin/livepreview.lua
@@ -14,3 +14,8 @@ vim.filetype.add({
 		[".*/live%-preview%.nvim/doc/.+%.txt"] = "help"
 	},
 })
+
+local plugin_path = require('livepreview.utils').get_plugin_path()
+if plugin_path and plugin_path:match("/nvim/pack") then
+	require('livepreview').setup()
+end
