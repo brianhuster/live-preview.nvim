3abe9b45fd35021df878143501be277b32830147
brianhuster <phambinhanctb2004@gmail.com>
2024-10-10 21:09:37 +0700

    update

--- lua/livepreview/server/Server.lua
+++ lua/livepreview/server/Server.lua
@@ -97,7 +97,6 @@ end
 --- @param ip string: IP address to bind to
 --- @param port number: port to bind to
 function Server:start(ip, port)
-	self:stop()
 	self.server:bind(ip, port)
 	self.server:listen(128, function(err)
 		if err then
