0a8f75fc8071ba679a0340c4eb3620bcedd91ba1
brianhuster <phambinhanctb2004@gmail.com>
2024-10-18 10:43:59 +0700

    update

--- static/mermaid/main.js
+++ static/mermaid/main.js
@@ -3,10 +3,25 @@ function renderMermaid() {
 		querySelector: '.language-mermaid',
 	});
 }
-mermaid.initialize({
-	startOnLoad: false,
-	securityLevel: 'loose',
-	theme: 'neutral',
-});
+
+const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+
+function handleColorSchemeChange(event) {
+	if (event.matches) {
+		mermaid.initialize({
+			startOnLoad: false,
+			securityLevel: 'loose',
+			theme: 'dark',
+		});
+	} else {
+		mermaid.initialize({
+			startOnLoad: false,
+			securityLevel: 'loose',
+			theme: 'default',
+		});
+	}
+}
+
 renderMermaid();
+mediaQueryList.addListener(handleColorSchemeChange);
 
