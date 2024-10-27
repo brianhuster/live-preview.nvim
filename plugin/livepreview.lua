beab832c6966803348fa7e6ea45d4a651f07f626
brianhuster <phambinhanctb2004@gmail.com>
2024-10-27 20:47:12 +0700

    fix health check

--- plugin/livepreview.lua
+++ plugin/livepreview.lua
@@ -1,8 +1,10 @@
 local health = require("livepreview.health")
 
 if not health.is_nvim_compatible() then
-	vim.notify_once(string.format([["Live Preview requires Nvim %s, but you are using Nvim %s"]],
-		health.supported_nvim_ver_range, health.nvim_ver))
+	vim.notify_once(
+		string.format([["live-preview.nvim requires Nvim %s, but you are using Nvim %s"]],
+			health.supported_nvim_ver_range,
+			health.nvim_ver), vim.log.levels.ERROR)
 end
 
 package.loaded["live-preview"] = require("livepreview")
