21b63d6b5d61b723b86c76a52cc6634b6f638044
brianhuster <phambinhanctb2004@gmail.com>
2024-10-29 16:01:43 +0700

    merge

--- lua/telescope/livepreview.lua
+++ lua/telescope/livepreview.lua
@@ -1,8 +1,13 @@
-local livepreview = require("livepreview")
-local telescope = require("telescope")
-local actions = require("telescope.actions")
-
 local M = {}
+
+local livepreview = require('livepreview')
+livepreview.utils = require('livepreview.utils')
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local conf = require('telescope.config').values
+
 
 M.setup = function(opts)
 	livepreview.setup(opts)
@@ -24,25 +29,39 @@ M.setup = function(opts)
 			},
 		},
 	})
-	vim.keymap.set("n", opts.keymap and opts.keymap.start or "<leader>lp", function()
-		require("telescope.builtin").find_files({
-			prompt_title = "Live Preview",
-			cwd = vim.fn.getcwd(),
-			attach_mappings = function(_, map)
-				map("i", "<CR>", function(prompt_bufnr)
-					local entry = require("telescope.actions.state").get_selected_entry()
-					actions.close(prompt_bufnr)
-					local filepath = entry.path
-					if livepreview.utils.supported_filetype(filepath) then
-						livepreview.preview_file(filepath, livepreview.config.port)
-					else
-						print("Selected file is not supported for live preview")
-					end
-				end)
-				return true
+	telescope.register_extension({
+		exports = {
+			livepreview_start = function()
+				pickers.new({}, {
+					prompt_title = "Live Preview",
+					finder = finders.new_oneshot_job({ "find", ".", "-type", "f" }, {}),
+					sorter = conf.generic_sorter({}),
+					attach_mappings = function(_, map)
+						map('i', '<CR>', function(prompt_bufnr)
+							local entry = require('telescope.actions.state').get_selected_entry()
+							actions.close(prompt_bufnr)
+							local filepath = entry.value
+							if livepreview.utils.supported_filetype(filepath) then
+								livepreview.preview_file(filepath, livepreview.config.port)
+							else
+								print("Selected file is not supported for live preview")
+							end
+						end)
+						return true
+					end,
+				}):find()
 			end,
-		})
-	end, { noremap = true, silent = true })
+		},
+	})
+
+	-- Picker for LivePreviewStop
+	telescope.register_extension({
+		exports = {
+			livepreview_stop = function()
+				livepreview.stop()
+			end,
+		},
+	})
 end
 
 return M
