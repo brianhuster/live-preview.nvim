ec176f507c4e9da721caebb7d105437ea6ac29e5
brianhuster <phambinhanctb2004@gmail.com>
2024-10-29 20:19:53 +0700

    add support for Telescope

--- lua/telescope/_extensions/livepreview.lua
+++ lua/telescope/_extensions/livepreview.lua
@@ -1,3 +1,4 @@
+local uv = vim.uv
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
@@ -6,53 +7,56 @@ local action_state = require("telescope.actions.state")
 local lp = require("livepreview")
 lp.utils = require("livepreview.utils")
 
-local function open()
-	local result = lp.utils.await_term_cmd("find . -type f").stdout
-
+local function find_files(directory)
 	local files = {}
-	local subdir_files = {}
-	for file in result:gmatch("[^\r\n]+") do
-		if lp.utils.supported_filetype(file) then
-			if file:match("^./[^/]+$") then
-				table.insert(files, file)
+	local function scan_dir(dir)
+		local handle = uv.fs_scandir(dir)
+		if not handle then return end
+		while true do
+			local name, type = uv.fs_scandir_next(handle)
+			if not name then break end
+			local filepath = dir .. '/' .. name
+			if type == 'directory' then
+				scan_dir(filepath)
 			else
-				table.insert(subdir_files, file)
+				if lp.utils.supported_filetype(filepath) then
+					table.insert(files, filepath)
+				end
 			end
 		end
 	end
+	scan_dir(directory)
+	return files
+end
 
-	for _, file in ipairs(subdir_files) do
-		table.insert(files, file)
-	end
-
-	pickers
-		.new({}, {
-			prompt_title = "Live Preview",
-			finder = finders.new_table({
-				results = files,
-			}),
-			sorter = conf.generic_sorter({}),
-			previewer = conf.file_previewer({}),
-			attach_mappings = function(_, map)
-				actions.select_default:replace(function(prompt_bufnr)
-					local entry = action_state.get_selected_entry()
-					actions.close(prompt_bufnr)
-					local filepath = entry.value
-					if lp.utils.supported_filetype(filepath) then
-						lp.preview_file(filepath, lp.config.port)
-						vim.cmd("edit " .. filepath)
-						lp.utils.open_browser(
-							string.format("http://localhost:%d/%s", lp.config.port, filepath),
-							lp.config.browser
-						)
-					else
-						print("Selected file is not supported for live-preview.nvim")
-					end
-				end)
-				return true
-			end,
-		})
-		:find()
+local function open()
+	local files = find_files(".")
+	pickers.new({}, {
+		prompt_title = "Live Preview",
+		finder = finders.new_table {
+			results = files,
+		},
+		sorter = conf.generic_sorter({}),
+		previewer = conf.file_previewer({}),
+		attach_mappings = function(_, map)
+			actions.select_default:replace(function(prompt_bufnr)
+				local entry = action_state.get_selected_entry()
+				actions.close(prompt_bufnr)
+				local filepath = entry.value
+				if lp.utils.supported_filetype(filepath) then
+					lp.preview_file(filepath, lp.config.port)
+					vim.cmd("edit " .. filepath)
+					lp.utils.open_browser(
+						string.format("http://localhost:%d/%s", lp.config.port, filepath),
+						lp.config.browser
+					)
+				else
+					print("Selected file is not supported for live-preview.nvim")
+				end
+			end)
+			return true
+		end,
+	}):find()
 end
 
 return require("telescope").register_extension({
