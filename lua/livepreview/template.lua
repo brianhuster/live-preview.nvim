8362a8271bb162fa155ee60d6073959e390df91d
brianhuster <phambinhanctb2004@gmail.com>
2024-12-22 18:07:11 +0700

    fix

--- lua/livepreview/template.lua
+++ lua/livepreview/template.lua
@@ -61,8 +61,8 @@ M.svg2html = function(svg)
 		<html lang="en">
 
 		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta charset="UTF-8"></meta>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 			<title>Live preview</title>
 		</head>
 
